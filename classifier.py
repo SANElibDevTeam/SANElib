6b592a151caf8eae8b8fda6fe91b4540a3d29a8e
Gabe Stechschulte <63432018+GStechschulte@users.noreply.github.com>
2021-01-12 11:42:33 +0100

    1d visualization function parameterization

--- classifier.py
+++ classifier.py
@@ -6,6 +6,10 @@ from sqlalchemy import create_engine
 from sqlalchemy import text
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
+from plotnine import ggplot, aes, geom_line, geom_point, labs, theme, element_text
+from plotnine.data import economics, mpg
+
+import plotnine as pn
 
 
 class SaneProbabilityEstimator:
@@ -179,38 +183,51 @@ class SaneProbabilityEstimator:
             self.model_id + '_m',
             Template(sql.tmplt['_m']).render(input=self))
 
-    def visual(self, feature1, target):
+    def visualize(self, feature1):
+
+        numFeats = self.numFeatures
+
+        if feature1 in numFeats:
+            index = self.numFeatures.index(feature1) + 1
+        else:
+            raise ValueError('Feature variable {} does not exist'.format(feature1))
+
+        for i in range(1, len(numFeats)+1):
+            if index == i:
+                feature1 = 'xn{}'.format(i)
+                bins = 'xq{}'.format(i)
+                minimum = 'mn{}'.format(i)
+                maximum = 'mx_{}'.format(i)
 
         hist = self.executeQuery('Discretization Histogram', '''
-            select * from {};'''.format(self.table_train))
-
-        cols = self.executeQuery('Fetching columns', '''
-            SHOW columns FROM {}'''.format(self.table_train))
-
-        cols = pd.DataFrame(cols)
-        names = []
-        for row in cols[0]:
-            names.append(row)
+            select distinct {} as xq, {} as mn, {} as mx, 
+            concat({}, ': ]', {}, ',', {}, ']') as bin, 
+            cast(y_ as char) as y_,
+            sum(nxy)over(partition by {}, y_)*1.0/
+            sum(nxy) over(partition by y_)  as p 
+            from {}_m 
+            order by {}, y_;'''.format(bins, minimum, maximum,
+                                        bins, minimum, maximum,
+                                        bins, self.model_id, bins))
 
         hist_df = pd.DataFrame(hist)
-        hist_df.reset_index()
-        hist_df.columns = names
+        hist_df.columns = ['xq', 'mn', 'mx', 'bin', 'y_', 'p']
+        hist_df[['p']] = hist_df[['p']].apply(pd.to_numeric)
+        t = hist_df[['bin']].drop_duplicates()
 
-        # 1d
-        #plt.hist(hist_df[feature1], bins=self.bins, density=True)
-        #plt.show()
-        hist_df[feature1].plot.kde()
-        plt.title('Probability Density Function of {}'.format(feature1))
-        plt.xlabel('{}'.format(feature1))
-        plt.ylabel('Density')
-        plt.show()
+        bin_list = hist_df['bin'].drop_duplicates().tolist()
+        bin_cat = pd.Categorical(hist_df['bin'], categories=bin_list)
 
-        # 2d
-        plt.hist2d(hist_df[target], hist_df[feature1], bins=self.bins, density=True)
-        plt.title('2d Density Plot')
-        plt.xlabel('{}'.format(target))
-        plt.ylabel('{}'.format(feature1))
-        plt.show()
+        p = (
+                ggplot(hist_df)
+                + aes(bin_cat, 'p', color='y_', group='y_')
+                + geom_point()
+                + geom_line()
+                + labs(y='p(qx|y)', x='bin', title='test')
+                + theme(axis_text_x=element_text(rotation=90, hjust=1))
+        )
+
+        print(p)
 
     def predict(self, table_eval):  # table_eval is the test set
         """
