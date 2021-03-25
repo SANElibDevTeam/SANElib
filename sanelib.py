a35c8d8f87a35a6171434d28603a21cfa1362c05
PatrickGiesserHSLU <patrick.giesser@stud.hslu.ch>
2021-03-25 13:48:26 +0100

    Added sanelib factory

---
+++ sanelib.py
@@ -0,0 +1,14 @@
+import lib
+import config as cons
+
+db = {
+    'drivername': 'mysql+mysqlconnector',
+    'host': cons.DB_HOST,
+    'port': cons.DB_PORT,
+    'username': cons.DB_USER,
+    'password': cons.DB_PW,
+    'database': cons.DB_NAME,
+    'query': {'charset': 'utf8'}
+}
+
+mdh = lib.mhd.MHD(db)
