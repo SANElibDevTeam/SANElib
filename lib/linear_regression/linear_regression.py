ddafefbf01c7af443674a96032c7f3550f1b529f
PatrickGiesserHSLU <patrick.giesser@stud.hslu.ch>
2021-03-26 11:20:51 +0100

    LinearRegression init, added pandas requirement

--- lib/linear_regression/linear_regression.py
+++ lib/linear_regression/linear_regression.py
@@ -1,3 +1,18 @@
+from util.database_connection import Database
+
+
 class LinearRegression:
-    def __init__(self):
+    def __init__(self, db):
+        self.db_connection = Database(db)
+
+    def estimate(self):
+        pass
+
+    def rank(self):
+        pass
+
+    def accuracy(self):
+        pass
+
+    def predict(self):
         pass
