9ec043f6ed8a5112e2f56b6a265e090d95dbe4d2
PatrickGiesserHSLU <patrick.giesser@stud.hslu.ch>
2021-04-08 14:03:08 +0200

    Implemented additional model functionality, added basic Exception messages

--- lib/linear_regression/linear_regression.py
+++ lib/linear_regression/linear_regression.py
@@ -2,6 +2,7 @@ from util.database_connection import Database
 from lib.linear_regression.model import Model
 import numpy as np
 from lib.linear_regression import sql_templates
+import sys
 
 
 class LinearRegression:
@@ -24,6 +25,10 @@ class LinearRegression:
         return self
 
     def load_model(self, model_id=None):
+        if model_id is None:
+            model_id = 'm0'
+        elif model_id not in self.get_model_list():
+            raise Exception('Provided model_id not found!')
         sql_statement = sql_templates.tmpl['get_all_from_where_id'].render(database=self.database, table='linreg_model',
                                                                            where_statement=model_id)
         data = np.asarray(self.db_connection.execute_query(sql_statement))[0]
@@ -37,7 +42,7 @@ class LinearRegression:
         self.model = Model(data[3], x_columns[:-1], y_column)
         self.model.id = data[0]
         self.model.name = data[1]
-        self.model.state = data[2]
+        self.model.state = int(data[2])
         self.model.prediction_table = data[4]
         self.model.prediction_columns = prediction_columns[:-1]
         self.model.input_size = int(data[8])
@@ -47,19 +52,27 @@ class LinearRegression:
 
     def get_model_list(self):
         sql_statement = sql_templates.tmpl['get_model_list'].render(database=self.database, table='linreg_model')
-        data = self.db_connection.execute_query(sql_statement)
+        try:
+            data = self.db_connection.execute_query(sql_statement)
+        except:
+            raise Exception('No models available!')
         model_list = [['ID', 'Name']]
         for x in data:
             model_list.append(x)
         return np.asarray(model_list)
 
     def get_model_description(self, model_id=None):
+        if self.model is None:
+            raise Exception('No model parameters available! Please load/create a model!')
         return "Model " + self.model.id + "\n" + "Name: " + self.model.name + "\n" + "Input table: " + self.model.input_table + "\n" + "X columns: " + str(
             self.model.x_columns) + "\n" + "Y column: " + str(self.model.y_column)
 
     def estimate(self, table=None, x_columns=None, y_column=None):
         if table is not None or x_columns is not None or y_column is not None:
             self.model = Model(table, x_columns, y_column)
+        elif self.model is None:
+            raise Exception(
+                'No model parameters available! Please load/create a model or provide table, x_columns and y_column as parameters to this function!')
         self.__add_ones_column()
         self.__init_calculation_table()
         self.__init_result_table()
@@ -72,21 +85,35 @@ class LinearRegression:
             sql_statement = sql_templates.tmpl['save_theta'].render(table="linreg_" + self.model.id + "_result",
                                                                     value=x)
             self.db_connection.execute(sql_statement)
-        self.__save_model()
+        if self.model.state < 1:
+            self.model.state = 1
+            self.__save_model()
         return self
 
     def score(self):
+        if self.model is None:
+            raise Exception('No model parameters available! Please load/create a model!')
+        elif self.model.state < 2:
+            raise Exception('No predictions available! Please use predict method first!')
         self.__init_score_table("linreg_" + self.model.id + "_score")
         sql_statement = sql_templates.tmpl['calculate_save_score'].render(table_id='linreg_' + self.model.id,
                                                                           input_table=self.model.input_table,
                                                                           y=self.model.y_column[0])
         self.db_connection.execute(sql_statement)
+        if self.model.state < 3:
+            self.model.state = 3
+            self.__save_model()
 
     def predict(self, table=None, x_columns=None):
+        if self.model is None:
+            raise Exception('No model parameters available! Please load/create a model!')
+        elif self.model.state < 1:
+            raise Exception('Model not trained! Please use estimate method first!')
         self.__init_prediction_table("linreg_" + self.model.id + "_prediction")
         if table is not None and x_columns is not None:
             self.model.prediction_table = table
             self.model.prediction_columns = x_columns
+            self.__save_model()
 
         input_table = self.model.prediction_table
         coefficients = self.get_coefficients()
@@ -98,21 +125,36 @@ class LinearRegression:
                                                              input_table=input_table,
                                                              prediction_statement=prediction_statement)
         self.db_connection.execute(sql_statement)
+        if self.model.state < 2:
+            self.model.state = 2
+            self.__save_model()
         return self
 
     def get_prediction_array(self):
+        if self.model is None:
+            raise Exception('No model parameters available! Please load/create a model!')
+        elif self.model.state < 2:
+            raise Exception('No predictions available! Please use predict method first!')
         sql_statement = sql_templates.tmpl['select_x_from'].render(x='y_prediction', database=self.database,
                                                                    table='linreg_' + self.model.id + '_prediction')
         data = self.db_connection.execute_query(sql_statement)
         return np.asarray(data)
 
     def get_coefficients(self):
+        if self.model is None:
+            raise Exception('No model parameters available! Please load/create a model!')
+        elif self.model.state < 1:
+            raise Exception('Model not trained! Please use estimate method first!')
         sql_statement = sql_templates.tmpl['select_x_from'].render(x='theta', database=self.database,
                                                                    table='linreg_' + self.model.id + '_result')
         data = self.db_connection.execute_query(sql_statement)
         return np.asarray(data)
 
     def get_score(self):
+        if self.model is None:
+            raise Exception('No model parameters available! Please load/create a model!')
+        elif self.model.state < 3:
+            raise Exception('No score available! Please use score method first!')
         sql_statement = sql_templates.tmpl['select_x_from'].render(x='score', database=self.database,
                                                                    table='linreg_' + self.model.id + '_score')
         data = self.db_connection.execute_query(sql_statement)
@@ -139,7 +181,8 @@ class LinearRegression:
         return self
 
     def __get_equations(self):
-        sql_statement = sql_templates.tmpl['get_all_from'].render(database=self.database, table="linreg_" + self.model.id + "_calculation")
+        sql_statement = sql_templates.tmpl['get_all_from'].render(database=self.database,
+                                                                  table="linreg_" + self.model.id + "_calculation")
         data = self.db_connection.execute_query(sql_statement)
         return np.asarray(data)
 
@@ -200,9 +243,12 @@ class LinearRegression:
             sum_statements = []
             for j in range(self.model.input_size + 1):
                 if j < self.model.input_size:
-                    sum_statements.append("sum(" + columns[i] + "*" + columns[j] + ") FROM " + self.model.input_table + "),")
+                    sum_statements.append(
+                        "sum(" + columns[i] + "*" + columns[j] + ") FROM " + self.model.input_table + "),")
                 else:
-                    sum_statements.append("sum(" + columns[i] + "*" + columns[j] + ") FROM " + self.model.input_table + ")")
-            sql_statement = sql_templates.tmpl['calculate_equations'].render(table='linreg_' + self.model.id + '_calculation', table_input=self.model.input_table,
-                                                                             sum_statements=sum_statements, x_columns=x)
+                    sum_statements.append(
+                        "sum(" + columns[i] + "*" + columns[j] + ") FROM " + self.model.input_table + ")")
+            sql_statement = sql_templates.tmpl['calculate_equations'].render(
+                table='linreg_' + self.model.id + '_calculation', table_input=self.model.input_table,
+                sum_statements=sum_statements, x_columns=x)
             self.db_connection.execute(sql_statement)
