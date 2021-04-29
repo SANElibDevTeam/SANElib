f101cd93fd8358347e40b0b1a34d921dd8d2c22f
PatrickGiesserHSLU <patrick.giesser@stud.hslu.ch>
2021-04-29 10:31:39 +0200

    Restructured multiply_matrices

--- util/database_functions/mysql.py
+++ util/database_functions/mysql.py
@@ -57,10 +57,43 @@ def multiply_matrices(database, table_a, table_b, result_table_name):
         if i < len(b_column_names) - 1:
             b_column_names_string = b_column_names_string + ","
 
-    # Init calculation table (nxm)
     n = len(a_column_names) - 1
     m = len(b_column_names) - 1
 
+    # Transpose tableA
+    sql_statement = tmpl['drop_table'].render(table="a_transposed")
+    database.execute(sql_statement)
+
+    number_of_a_columns = len(a_column_names) - 1
+    transposed_a_columns = []
+    for i in range(n):
+        transposed_a_columns.append("x" + str(i + 1))
+    sql_statement = tmpl['init_table'].render(database=database.database_name, table="a_transposed",
+                                                  x_columns=transposed_a_columns)
+    database.execute(sql_statement)
+
+    x_column_string = ""
+    for i in range(n):
+        x_column_string = x_column_string + "x" + str(i + 1)
+        if i < n - 1:
+            x_column_string = x_column_string + ","
+
+    for i in range(n):
+        selection_string = ""
+        for j in range(number_of_a_columns):
+            if j == 0:
+                limit = "1"
+            else:
+                limit = str(j) + "," + str(j)
+            selection_string = selection_string + "(SELECT x" + str(i + 1) + " FROM " + str(
+                table_a) + " LIMIT " + limit + ")"
+            if j < number_of_a_columns - 1:
+                selection_string = selection_string + ","
+        sql_statement = tmpl['transpose_table'].render(table="a_transposed", x_column_string=x_column_string,
+                                                           selection_string=selection_string)
+        database.execute(sql_statement)
+
+    # Init calculation table (nxm)
     sql_statement = tmpl['drop_table'].render(table="matmul_calculation")
     database.execute(sql_statement)
 
@@ -83,40 +116,9 @@ def multiply_matrices(database, table_a, table_b, result_table_name):
                                                                 a_columns=a_column_names,
                                                                 b_columns=b_column_names, table_a=table_a,
                                                                 table_b=table_b)
-    print(sql_statement)
-
-    # Transpose tableA
-    sql_statement = tmpl['drop_table'].render(table="a_transposed")
     database.execute(sql_statement)
 
-    number_of_a_columns = len(get_column_names(database, table_a)) - 1
-    x_columns = []
-    for i in range(n):
-        x_columns.append("x" + str(i + 1))
-    sql_statement = tmpl['init_table'].render(database=database.database_name, table="a_transposed",
-                                              x_columns=x_columns)
-    database.execute(sql_statement)
 
-    x_column_string = ""
-    for i in range(n):
-        x_column_string = x_column_string + "x" + str(i + 1)
-        if i < n - 1:
-            x_column_string = x_column_string + ","
-
-    for i in range(n):
-        selection_string = ""
-        for j in range(number_of_a_columns):
-            if j == 0:
-                limit = "1"
-            else:
-                limit = str(j) + "," + str(j)
-            selection_string = selection_string + "(SELECT x" + str(i + 1) + " FROM " + str(
-                table_a) + " LIMIT " + limit + ")"
-            if j < number_of_a_columns - 1:
-                selection_string = selection_string + ","
-        sql_statement = tmpl['transpose_table'].render(table="a_transposed", x_column_string=x_column_string,
-                                                       selection_string=selection_string)
-        database.execute(sql_statement)
 
     # Calculate results
     # Init result_table, calculate results
