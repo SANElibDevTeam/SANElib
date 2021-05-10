e118e599f0ea76066ce53923a3e60c1c0848761c
PatrickGiesserHSLU <patrick.giesser@stud.hslu.ch>
2021-05-10 20:00:15 +0200

    Various fixes part1

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import sanelib
 from util.database_functions.mysql import multiply_matrices
 from lib.linear_regression.example import run_bmi_example
+from lib.mdh.example import run
 from util import timer
 
 # Run MDH example
-# mdh = sanelib.mdh
-# mdh_example.run(mdh)
+mdh = sanelib.mdh
+run(mdh)
+
+# Run LinearRegression example
+# run_bmi_example()
+
