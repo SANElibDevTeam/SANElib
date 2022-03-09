65155754fc1d2e46d6b2e0a56ba54f76a1e5c6a9
Anna Huber <annamagdalenahuber@gmail.com>
2022-03-09 13:57:16 +0100

    added gaussian classifier to init

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,3 +2,4 @@ from lib.dtc import DecisionTreeClassifier
 from lib.kmeans import kmeans
 from lib.linear_regression import linear_regression
 from lib.mdh import mdh
+from lib.gaussian_classifier import gaussian_classifier
