eaab28f94a6088a5685d7c7e49b31da8b4b20548
Anna Huber <annamagdalenahuber@gmail.com>
2022-04-01 17:06:03 +0200

    added timer to example

--- lib/gaussian_classifier/example.py
+++ lib/gaussian_classifier/example.py
@@ -1,7 +1,8 @@
 import sanelib
-
+import util.timer as timer
 
 def run_example(table_name="covtyp", prediction_table_name="prediction_covtyp"):
+    timer.start()
     gc = sanelib.gc
     gc.set_log_level("DEBUG")
 
@@ -10,6 +11,6 @@ def run_example(table_name="covtyp", prediction_table_name="prediction_covtyp"):
     x_columns = ['Elevation','Horizontal_Distance_To_Fire_Points']
     y_column = ['Cover_Type']
     gc.estimate(table_name, x_columns, y_column)
-
+    timer.end()
 if __name__ == '__main__':
     run_example()
